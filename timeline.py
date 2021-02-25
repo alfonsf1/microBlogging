def97c10f61ef098264298e423536b810d387f66
RyanLuong1 <ryanluong83@gmail.com>
2021-02-24 18:29:31 -0800

    Forgot to add def to functions

--- timeline.py
+++ timeline.py
@@ -1,16 +1,16 @@
-getUserTimeline(username):
+def getUserTimeline(username):
     #first check if user is in db
     #if not, return error
     #else find the post with the most recent date
-getPublicTimelinme():
+def getPublicTimelinme():
     #first, check if user is in db
     #if not, return error
     #else return all the post from that user
-getHomeTimeline(username):
+def getHomeTimeline(username):
     #first, checkf if user is in db
     #if not, return error
     #else return recent posts from all the users that this user is following
-postTweet(username, text):
+def postTweet(username, text):
     #first check if the ussername exist
     #if not, error
     #else, post the new tweet
