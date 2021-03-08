b93e189b6319165bc5a0405ae7c497432ec7648f
alfonsf1 <figueroa.a@csu.fullerton.edu>
2021-03-07 21:55:37 -0800

    updating sql inserts

--- timeline.sql
+++ timeline.sql
@@ -13,12 +13,31 @@ INSERT INTO post(postUserID, author, postText) VALUES(1, "Ryan", "Hi im Ryan");
 INSERT INTO post(postUserID, author, postText) VALUES(2, "Pure", "Hi Im Pure");
 INSERT INTO post(postUserID, author, postText) VALUES(3, "Alfonso", "Hi! Im Alfonso");
 INSERT INTO post(postUserID, author, postText) VALUES(4, "Yash", "Hi! Im Yash");
-INSERT INTO post(postUserID, author, postText) VALUES(5, "Rosendo", "Hi! Im Rosendo!");
-INSERT INTO post(postUserID, author, postText) VALUES (3, "Alfonso", "Bye Alfonso");
-INSERT INTO post(postUserID, author, postText) VALUES (2, "Pure", "Bye Pure");
+INSERT INTO post(postUserID, author, postText) VALUES(5, "Valerie", "Hi! Im Rosendo!");
+INSERT INTO post(postUserID, author, postText) VALUES(6, "Caitlyn", "Hi! Im Rosendo!");
+INSERT INTO post(postUserID, author, postText) VALUES(7, "Morgana", "Hi! Im Rosendo!");
 
-
-
+INSERT INTO post(postUserID, author, postText) VALUES(1, "Ryan", "C++ is fun");
+INSERT INTO post(postUserID, author, postText) VALUES(2, "Pure", "I like football");
+INSERT INTO post(postUserID, author, postText) VALUES(5, "Valerie", "I like running");
+INSERT INTO post(postUserID, author, postText) VALUES(6, "Caitlyn", "I like games");
+INSERT INTO post(postUserID, author, postText) VALUES(1, "Ryan", "I like Soccer");
+INSERT INTO post(postUserID, author, postText) VALUES(6, "Caitlyn", "sandwhiches is my favorite food");
+INSERT INTO post(postUserID, author, postText) VALUES(4, "Yash", "pasta is my favorite food");
+INSERT INTO post(postUserID, author, postText) VALUES(5, "Valerie", "soup is my favorite food");
+INSERT INTO post(postUserID, author, postText) VALUES(4, "Yash", "Cant wait to go back to work");
+INSERT INTO post(postUserID, author, postText) VALUES(3, "Alfonso", "I like Tennis");
+INSERT INTO post(postUserID, author, postText) VALUES(4, "Yash", "I like Swimming");
+INSERT INTO post(postUserID, author, postText) VALUES(1, "Ryan", "pizza is my favorite food");
+INSERT INTO post(postUserID, author, postText) VALUES(2, "Pure", "hamburgers is my favorite food");
+INSERT INTO post(postUserID, author, postText) VALUES(3, "Alfonso", "cheeseburgers is my favorite food");
+INSERT INTO post(postUserID, author, postText) VALUES(5, "Valerie", "Is disneyland open yet?");
+INSERT INTO post(postUserID, author, postText) VALUES(2, "Pure", "Soaking up the sun today");
+INSERT INTO post(postUserID, author, postText) VALUES(3, "Alfonso", "Going to san diego this week!");
+INSERT INTO post(postUserID, author, postText) VALUES(6, "Caitlyn", "Watching Netflix!");
+INSERT INTO post(postUserID, author, postText) VALUES(7, "Morgana", "salmon is my favorite food");
+INSERT INTO post(postUserID, author, postText) VALUES(7, "Morgana", "I like stuns");
+INSERT INTO post(postUserID, author, postText) VALUES(7, "Morgana", "Its finally raining!");
 
 
 
--- user.sql
+++ user.sql
@@ -26,13 +26,19 @@ INSERT INTO user VALUES(2, "Pure", "pure@gmail.com", "395i9g");
 INSERT INTO user VALUES(3, "Alfonso", "alfonso@gmail.com", "395i9g");
 INSERT INTO user VALUES(4, "Yash", "yash@gmail.com", "395i9g");
 INSERT INTO user VALUES(5, "Rosendo", "rosendo@gmail.com", "395i9g");
+INSERT INTO user VALUES(6, "Valerie", "valerie@gmail.com", "395i9g");
+INSERT INTO user VALUES(7, "Caitlyn", "Caitlyn@gmail.com", "395i9g");
+INSERT INTO user VALUES(8, "Morgana", "rosendo@gmail.com", "395i9g");
 
 INSERT INTO followers(userID, followingID) VALUES(1, 2);
 INSERT INTO followers(userID, followingID) VALUES(2, 5);
 INSERT INTO followers(userID, followingID) VALUES(3, 1);
 INSERT INTO followers(userID, followingID) VALUES(4, 2);
 INSERT INTO followers(userID, followingID) VALUES(5, 4);
-
-
+INSERT INTO followers(userID, followingID) VALUES(1, 2);
+INSERT INTO followers(userID, followingID) VALUES(1, 3);
+INSERT INTO followers(userID, followingID) VALUES(3, 7);
+INSERT INTO followers(userID, followingID) VALUES(7, 2);
+INSERT INTO followers(userID, followingID) VALUES(8, 1);
 
 
